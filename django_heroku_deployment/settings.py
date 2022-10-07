1a01b28345a1c8cf9f0f5123bcf77de46482deba
Steven Mendoza <smendoza171@gmail.com>
2022-10-06 19:35:26 -0700

    test

--- django_heroku_deployment/settings.py
+++ django_heroku_deployment/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-4d$*ofva4&e8*98e1f=_k6yka6)1fsrirza2z66dznjto#4)bv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        'mendozadeploy1.herokuapp.com'
+]
 
 
 # Application definition
